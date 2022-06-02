7e18baf760d0812120e12d1f7a1e64f1cbc71adf
gentstats <danielplan91@gmail.com>
2022-06-02 11:02:20 +0200

    Create redux store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Provider } from 'react-redux';
 import RestaurantScreen from './components/RestaurantScreen';
+import store from './store';
 
 function App() {
   return (
     <div>
-      <RestaurantScreen />
+      <Provider store={store}>
+        <RestaurantScreen />
+      </Provider>
     </div>
   );
 }
